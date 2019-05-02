73977ec5456733ad42e3792803dc0814c81e8c47
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-05-02 12:30:10 +0200

    Run scripts

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
-	"log"
+	"os"
 	"path"
 
 	"github.com/bitrise-io/go-utils/command/git"
 	"github.com/bitrise-io/go-utils/errorutil"
+	"github.com/bitrise-io/go-utils/log"
 	_ "github.com/lib/pq"
 )
 
@@ -27,7 +28,7 @@ func cloneRepo() (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("failed to create temp dir")
 	}
-	fmt.Printf("cloning into: %s\n", dir)
+	log.Printf("cloning into: %s\n", dir)
 
 	repo, err := git.New(dir)
 	if err != nil {
@@ -43,6 +44,59 @@ func cloneRepo() (string, error) {
 		return "", fmt.Errorf("failed to execute clone command, error: %s", err)
 	}
 	return dir, nil
+}
+
+func connectToDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Successfully connected!")
+	return db, nil
+}
+
+func runSQLStatement(db *sql.DB, statement string) error {
+	_, err := db.Exec(statement)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func testQuery(db *sql.DB) error {
+	type Table struct {
+		schemaname  string
+		tablename   string
+		tableowner  string
+		tablespace  *string
+		hasindexes  bool
+		hasrules    bool
+		hastriggers bool
+		rowsecurity bool
+	}
+	sqlStatement := `SELECT * FROM pg_catalog.pg_tables;`
+	var table Table
+	row := db.QueryRow(sqlStatement)
+	err := row.Scan(&table.schemaname, &table.tablename, &table.tableowner, &table.tablespace,
+		&table.hasindexes, &table.hasrules, &table.hastriggers, &table.rowsecurity)
+	switch err {
+	case sql.ErrNoRows:
+		log.Printf("No rows were returned!")
+		return nil
+	case nil:
+		log.Printf("%v", table)
+	default:
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -68,43 +122,34 @@ func main() {
 	log.Printf("Scripts to run: %s", scripts)
 
 	// Connect to DB
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := connectToDB()
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Warnf("failed to close DB")
+		}
+	}()
 
-	err = db.Ping()
+	err = testQuery(db)
 	if err != nil {
-		panic(err)
+		log.Warnf("failed to list tables, error: %s", err)
 	}
-	fmt.Println("Successfully connected!")
 
-	type Table struct {
-		schemaname  string
-		tablename   string
-		tableowner  string
-		tablespace  *string
-		hasindexes  bool
-		hasrules    bool
-		hastriggers bool
-		rowsecurity bool
-	}
-	sqlStatement := `SELECT * FROM pg_catalog.pg_tables;`
-	var table Table
-	row := db.QueryRow(sqlStatement)
-	err = row.Scan(&table.schemaname, &table.schemaname, &table.tableowner, &table.tablespace,
-		&table.hasindexes, &table.hasrules, &table.hastriggers, &table.rowsecurity)
-	switch err {
-	case sql.ErrNoRows:
-		fmt.Println("No rows were returned!")
-		return
-	case nil:
-		fmt.Println(table)
-	default:
-		panic(err)
+	for _, script := range scripts {
+		file, err := os.Open(script)
+		if err != nil {
+			panic(fmt.Errorf("failed to open file, error: %s", err))
+		}
+		sqlStatement, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(fmt.Errorf("failed to read file content, error: %s", err))
+		}
+		err = runSQLStatement(db, string(sqlStatement))
+		if err != nil {
+			log.Warnf("failed to run script: %s, error: %s", script, err)
+		}
 	}
 }
